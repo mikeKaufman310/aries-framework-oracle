860ff2b7b1a3ab493ffa15a34b59fc256147af2a
Adrian Ross <amr424@lehigh.edu>
2023-12-01 00:34:30 -0500

    Add DID URL parsing capabilities

--- src/dids/identifiers.ts
+++ src/dids/identifiers.ts
@@ -4,10 +4,20 @@ import { isBase58 } from "class-validator";
 
 const ID_CHAR = "[a-zA-Z]";
 const IDENTIFIER = `(${ID_CHAR}+)`;
+const PATH = `(/[^#?]*)?`;
+const QUERY_PARAM = `([^#&=?]+=[^#&=?]+)`;
+const QUERY = `([?](?:${QUERY_PARAM}&)*)?`;
+const FRAGMENT = `(#([^?]*))?`;
 
-export const orclIdentifierRegex = new RegExp(`^did:orcl:${IDENTIFIER}$`);
+export const orclIdentifierRegex = new RegExp(
+  `^did:orcl:${IDENTIFIER}${PATH}${QUERY}${FRAGMENT}$`
+);
 
-export type ParsedOracleDid = ParsedDid;
+export type ParsedOracleDid = ParsedDid & {
+  path?: string;
+  params?: Record<string, string>;
+  fragment?: string;
+};
 
 export function parseOracleDid(didUrl: string): ParsedOracleDid | null {
   if (!didUrl) {
@@ -26,9 +36,29 @@ export function parseOracleDid(didUrl: string): ParsedOracleDid | null {
       didUrl,
     };
 
+    if (sections[2]) {
+      parts.path = sections[2];
+    }
+
+    if (sections[3]) {
+      const params = sections[3].slice(1).split("&");
+      parts.params = {};
+
+      for (const param of params) {
+        const [key, value] = param.split("=");
+        parts.params[key] = value;
+      }
+    }
+
+    if (sections[4]) {
+      parts.fragment = sections[4].slice(1);
+    }
+
     return parts;
   } else {
-    console.log("Not a valid Uuid");
+    console.error(
+      "Not a valid Oracle DID. Check the format and components of the DID."
+    );
   }
 
   return null;
