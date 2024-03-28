28dd2c99bbe05e27b6d3d9dae8bb185a5d991f38
mikeKaufman310 <mpk225@lehigh.edu>
2024-03-28 12:07:33 -0400

    created driver class and config files directory structure

---
+++ src/dids/OracleResolveDriver.ts
@@ -0,0 +1,11 @@
+import type {DidResolver} from '@credo-ts/core'
+
+/**
+ * Class to implemented a DID Resolver Driver to be a part of DIF Universal Resolver 
+ * @author Michael Kaufman
+ * @summary Implements a DID Resolver Driver using previously implemented functions in 
+ * Oracle codebase, as well as using functions from Open-Wallet Credo's typescript DID libraries
+ */
+export class OracleResolveDriver implements DidResolver{
+
+}
